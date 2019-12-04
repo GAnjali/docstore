08b7c9258a60f5f0e0dc8327ca6eb275677d96ff
ganjali <gaddam.anjali@oracle.com>
2019-12-04 21:54:01 +0530

    Create DB schema for creating a user table, modify files to have relation with user table

--- db/schema.js
+++ db/schema.js
@@ -11,8 +11,8 @@ pool.on('connect', () => {
     console.log('connected to db')
 });
 
-const createTables = () => {
-    const queryText = "CREATE TABLE IF NOT EXISTS FILES(id UUID PRIMARY KEY, name VARCHAR(30) UNIQUE NOT NULL , content VARCHAR(100000))";
+const createFileTable    = () => {
+    const queryText = `CREATE TABLE IF NOT EXISTS files(id UUID PRIMARY KEY, name VARCHAR(30) UNIQUE NOT NULL, content VARCHAR(100000), owner_id UUID NOT NULL, FOREIGN KEY (owner_id) REFERENCES users (id) ON DELETE CASCADE)`;
 
     pool.query(queryText)
         .then((res) => {
@@ -25,11 +25,68 @@ const createTables = () => {
         })
 };
 
+const createUserTable = () => {
+    const queryText =`CREATE TABLE IF NOT EXISTS users( id UUID PRIMARY KEY, email VARCHAR(128) UNIQUE NOT NULL, password VARCHAR(128) NOT NULL)`;
+
+    pool.query(queryText)
+        .then((res) => {
+            console.log(res);
+            pool.end();
+        })
+        .catch((err) => {
+            console.log(err);
+            pool.end();
+        });
+};
+
+const dropFileTable = () => {
+    const queryText = 'DROP TABLE IF EXISTS files returning *';
+    pool.query(queryText)
+        .then((res) => {
+            console.log(res);
+            pool.end();
+        })
+        .catch((err) => {
+            console.log(err);
+            pool.end();
+        });
+};
+
+const dropUserTable = () => {
+    const queryText = 'DROP TABLE IF EXISTS users returning *';
+    pool.query(queryText)
+        .then((res) => {
+            console.log(res);
+            pool.end();
+        })
+        .catch((err) => {
+            console.log(err);
+            pool.end();
+        });
+};
+
+const createAllTables = () => {
+    createUserTable();
+    createFileTable();
+};
+
+const dropAllTables = () => {
+    dropUserTable();
+    dropFileTable();
+};
+
 pool.on('remove', () => {
     console.log("client removed");
     process.exit(0);
 });
 
-module.exports = {createTables};
+module.exports = {
+    createFileTable,
+    createUserTable,
+    createAllTables,
+    dropUserTable,
+    dropFileTable,
+    dropAllTables
+};
 
 require('make-runnable');
