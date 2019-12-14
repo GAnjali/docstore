747cd6e9f6d1ef98b2b1698b60a312e4f284afcd
ganjali <gaddam.anjali@oracle.com>
2019-12-14 17:14:26 +0530

    Add Header to the Home page

---
+++ client/src/Pages/Home/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './Home.css';
+
+const Header = () => {
+    return (
+        <nav className="navbar">
+            <h2>Docstore</h2>
+        </nav>
+    );
+}
+
+export default Header;
--- client/src/Pages/Home/index.js
+++ client/src/Pages/Home/index.js
@@ -1,7 +1,42 @@
-import React from "react";
+import React, {Component} from "react";
+import {getToken} from "../../Util/localStorageUtil";
+import decode from "jwt-decode";
+import Header from "./Header";
+import './Home.css';
 
-function Home(props) {
-    return <div>Home Page</div>;
+class Home extends Component {
+
+    state = {
+        folders: []
+    };
+
+    componentDidMount() {
+        if (!this.isLoggedIn()) {
+            this.props.history.replace('/login')
+        }
+    }
+
+    isLoggedIn = () => {
+        const token = getToken();
+        return !!token && !this.isTokenExpired(token)
+    };
+
+    isTokenExpired = (token) => {
+        try {
+            const decoded = decode(token);
+            return decoded.exp < Date.now() / 1000;
+        } catch (err) {
+            return false;
+        }
+    };
+
+    render() {
+        if (this.state.folders) {
+            return (
+                <Header />
+            )
+        }
+    }
 }
 
 export default Home;
