fc9dc368bbf0650605cc8f186bfc1c230107065a
GAnjali <anjali.gaddam98@gmail.com>
2019-12-15 23:00:53 +0530

    Add getFiles functionality to the Home component, refactor fetchDocs functionality to fetch Folders and Files

--- client/src/Pages/Home/index.js
+++ client/src/Pages/Home/index.js
@@ -4,7 +4,7 @@ import decode from "jwt-decode";
 import Header from "./Header";
 import './Home.css';
 import Sidebar from "./Sidebar";
-import {getFolders} from "./HomeService";
+import {getFiles, getFolders} from "./HomeService";
 import MainSection from "./MainSection";
 
 class Home extends Component {
@@ -15,24 +15,36 @@ class Home extends Component {
 
     state = {
         folders: [],
+        files: [],
         error: ''
     };
 
     componentDidMount() {
         if (!this.isLoggedIn()) {
             this.props.history.replace('/login')
-        } else this.fetchDocs();
+        } else {
+            this.fetchDocs('Folders', 0);
+            this.fetchDocs('Files', 0);
+        }
     }
 
-    fetchDocs = async () => {
-        const foldersResponse = await getFolders(0);
-        if (foldersResponse.status === 200) {
-            this.setState({
-                folders: foldersResponse.data.data
-            })
+    fetchDocs = async (docType, parentFolderId) => {
+        const docsResponse = (docType === 'Folders') ? await getFolders(parentFolderId) : await getFiles(parentFolderId);
+        if (docsResponse.status === 200) {
+            if (docType === 'Folders' && docsResponse.data.data !== undefined) {
+                this.setState({
+                    folders: docsResponse.data.data
+                });
+            } else {
+                if (docsResponse.data.data !== undefined) {
+                    this.setState({
+                        files: docsResponse.data.data
+                    });
+                }
+            }
         } else {
             this.setState({
-                error: foldersResponse.message
+                error: docsResponse.message
             })
         }
     };
@@ -52,15 +64,13 @@ class Home extends Component {
     };
 
     render() {
-        if (this.state.folders) {
-            return (
-                <>
-                    <Header/>
-                    <Sidebar/>
-                    <MainSection folders={this.state.folders}/>
-                </>
-            )
-        }
+        return (
+            <>
+                <Header/>
+                <Sidebar/>
+                <MainSection folders={this.state.folders}/>
+            </>
+        )
     }
 }
 
