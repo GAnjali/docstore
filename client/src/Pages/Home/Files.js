0e579e65d24ae69b55d27abd4d58a347a35330fa
GAnjali <anjali.gaddam98@gmail.com>
2019-12-16 07:14:53 +0530

    Add css styles to the Files container

--- client/src/Pages/Home/Files.js
+++ client/src/Pages/Home/Files.js
@@ -1,9 +1,18 @@
 import React from "react";
+import fileIcon from "../../Util/fileicon.jpg"
 
 const Files = (props) => {
     return (
-        <div>
-            {props.files.toString()};
+        <div className={"files-block"}>
+            <span className={"heading"}> Files </span>
+            <hr/>
+            {props.files.map((file, i) => (
+                <div className={"file"} key={i}>
+                    <img src={fileIcon} className={"file-icon"}/>
+                    <hr/>
+                    <p className={"file-title"}>{file.name}</p>
+                </div>
+            ))}
         </div>
     )
 };
