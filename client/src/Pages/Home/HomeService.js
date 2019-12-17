588bb868735a220407e7ec6f6cdb96d95248964c
GAnjali <anjali.gaddam98@gmail.com>
2019-12-17 14:22:04 +0530

    Add sharing http request to backend service to HomeService.js

--- client/src/Pages/Home/HomeService.js
+++ client/src/Pages/Home/HomeService.js
@@ -51,3 +51,27 @@ export const deleteFile = async (id) => {
         return e;
     }
 };
+
+export const getUserByEmail = async (email) => {
+    try {
+        console.log(email);
+        const response = await axios.get(process.env.REACT_APP_SERVER_URL + '/users/' + email, config);
+        return response;
+    } catch (e) {
+        return e;
+    }
+};
+
+export const addShare = async (fileid, sharetype, userid) => {
+    const input = {
+        fileid: fileid,
+        userid: userid,
+        sharetype: sharetype
+    };
+    try {
+        const response = await axios.post(process.env.REACT_APP_SERVER_URL + '/shares/', input, config);
+        return response;
+    } catch (e) {
+        return e;
+    }
+};
