c7e4f9d35503c66f84cbe1407ab81adf58e08b09
GAnjali <anjali.gaddam98@gmail.com>
2019-12-18 12:33:15 +0530

    Add tests for create file, update file, delete file, get all files API endpoints

--- server/api/test/file.test.js
+++ server/api/test/file.test.js
@@ -7,98 +7,155 @@ chai.use(chatHttp);
 const {expect} = chai;
 
 describe('Test the File endpoints', () => {
-    it('Should get all files', (done) => {
-        chai.request(app)
-            .get('/files')
-            .set('Accept', 'application/json')
-            .end((err, res) => {
-                expect(res.status).to.equal(200);
-                if (res.body.data && (res.body.data).length > 0) {
-                    res.body.data.should.be.a('Array');
-                    res.body.data[0].should.have.property('id');
-                    res.body.data[0].should.have.property('name');
-                    res.body.data[0].should.have.property('content');
-                } else {
-                    expect(res.body.message).to.equals("No file found");
-                }
-                done();
-            });
+
+    describe('get all files', () => {
+
+        let token, newUser, newFile;
+        before(() => {
+            newUser = {
+                email: 'testgetFolderAPIs@gmail.com',
+                password: 'test1'
+            };
+            newFile = {
+                name: 'testgetallfiles'
+            };
+        });
+
+        it('Should get all the files', async () => {
+            await chai.request(app).post('/users').set('Accept', 'application/json').send(newUser).then(async () => {
+                await chai.request(app).post('/users/login').set('Accept', 'application/json').send(newUser).then(async (res) => {
+                    token = res.body.data;
+                    await chai.request(app).post('/files').set('Accept', 'application/json').set('x-access-token', token).send(newFile).then(async (res1) => {
+                        await chai.request(app).get('/files').set('Accept', 'application/json').set('x-access-token', token).then((response) => {
+                            expect(response.status).to.equal(200);
+                            expect(response.body.message).to.include("Files retrieved");
+                            expect(response.body.data[0].name).to.include(newFile.name);
+                        });
+                    })
+                })
+            })
+        });
     });
 
-    it('Should create a file', (done) => {
-        const file = {
-            name: 'Test file',
-            content: 'This is a test file in docstore app'
-        };
-        chai.request(app)
-            .post('/files')
-            .set('Accept', 'application/json')
-            .send(file)
-            .end((err, res) => {
-                expect(res.status).to.equal(201);
-                expect(res.body.data).to.include({
-                    name: file.name,
-                    content: file.content
-                });
-                done();
+    describe('create file', () => {
+
+        let token, newUser, newFile;
+
+        beforeEach(() => {
+            newUser = {
+                email: 'testcreateFileAPIs@gmail.com',
+                password: 'test1'
+            };
+            newFile = {
+                name: 'testfile2'
+            };
+        });
+
+        it('Should create a file', async () => {
+            await chai.request(app).post('/users').set('Accept', 'application/json').send(newUser).then(async () => {
+                await chai.request(app).post('/users/login').set('Accept', 'application/json').send(newUser).then(async (res) => {
+                    token = res.body.data;
+                    await chai.request(app).post('/files').set('Accept', 'application/json').set('x-access-token', token).send(newFile).then((response) => {
+                        expect(response.status).to.equal(201);
+                        expect(response.body.message).to.equals("File Added!");
+                    });
+                })
+            })
+        });
+
+        it('Should not create a file without token', async () => {
+            await chai.request(app).post('/files').set('Accept', 'application/json').send(newFile).then((response) => {
+                expect(response.status).to.equal(400);
+                expect(response.body.message).to.equals("Token is not provided");
             });
+        });
+
+        it('Should not create a file without filename', async () => {
+            newUser.email = "testFileAPIs1@gmail.com";
+            const newFile = {};
+            await chai.request(app).post('/users').set('Accept', 'application/json').send(newUser).then(async () => {
+                await chai.request(app).post('/users/login').set('Accept', 'application/json').send(newUser).then(async (res) => {
+                    token = res.body.data;
+                    await chai.request(app).post('/files').set('Accept', 'application/json').set('x-access-token', token).send(newFile).then((response) => {
+                        expect(response.status).to.equal(400);
+                        expect(response.body.message).to.include("notNull Violation");
+                    });
+                })
+            })
+        });
+
+        it('Should not create a file with duplicate filename', async () => {
+            newFile.name = "testfile2";
+            await chai.request(app).post('/users').set('Accept', 'application/json').send(newUser).then(async () => {
+                await chai.request(app).post('/users/login').set('Accept', 'application/json').send(newUser).then(async (res) => {
+                    token = res.body.data;
+                    await chai.request(app).post('/files').set('Accept', 'application/json').set('x-access-token', token).send(newFile).then((response) => {
+                        expect(response.status).to.equal(400);
+                        expect(response.body.message).to.include("File name already in use");
+                    });
+                })
+            })
+        });
     });
 
-    it('Should not create a file with incomplete parameters', (done) => {
-        const file = {};
-        chai.request(app)
-            .post('/files')
-            .set('Accept', 'application/json')
-            .send(file)
-            .end((err, res) => {
-                expect(res.status).to.equal(400);
-                done();
-            });
+    describe('delete file', () => {
+
+        let token, newUser, newFile;
+        before(() => {
+            newUser = {
+                email: 'testdeleteFileAPIs@gmail.com',
+                password: 'test1'
+            };
+            newFile = {
+                name: 'testdeletefile2'
+            };
+        });
+
+        it('Should delete file by id', async () => {
+            await chai.request(app).post('/users').set('Accept', 'application/json').send(newUser).then(async () => {
+                await chai.request(app).post('/users/login').set('Accept', 'application/json').send(newUser).then(async (res) => {
+                    token = res.body.data;
+                    await chai.request(app).post('/files').set('Accept', 'application/json').set('x-access-token', token).send(newFile).then(async (response) => {
+                        const fileid = response.body.data.id;
+                        await chai.request(app).delete('/files/' + fileid).set('Accept', 'application/json').set('x-access-token', token).then((response) => {
+                            expect(response.status).to.equal(200);
+                            expect(response.body.message).to.include("File deleted");
+                        });
+                    })
+                })
+            })
+        });
     });
 
-    it('Should get a particular file', (done) => {
-        const fileId = 1;
-        chai.request(app)
-            .get(`/files/${fileId}`)
-            .set('Accept', 'application/json')
-            .end((err, res) => {
-                expect(res.status).to.equal(200);
-                res.body.data.should.have.property('id');
-                res.body.data.should.have.property('name');
-                res.body.data.should.have.property('content');
-                done();
-            });
-    });
+    describe('update file', () => {
 
-    it('Should update a file', (done) => {
-        const fileId = 1;
-        const updatedFile = {
-            id: fileId,
-            name: 'Update f',
-            content: 'update file content'
-        };
-        chai.request(app)
-            .put(`/files/${fileId}`)
-            .set('Accept', 'application/json')
-            .send(updatedFile)
-            .end((err, res) => {
-                expect(res.status).to.equal(200);
-                expect(res.body.data.id).equal(updatedFile.id);
-                expect(res.body.data.name).equal(updatedFile.name);
-                expect(res.body.data.content).equal(updatedFile.content);
-                done();
-            });
-    });
+        let token, newUser, newFile;
+        before(() => {
+            newUser = {
+                email: 'testupdateFileAPIs@gmail.com',
+                password: 'test1'
+            };
+            newFile = {
+                name: 'testupdatefile2',
+                content: "test file content"
+            };
+        });
 
-    it('Should delete a file', (done) => {
-        const fileId = 1;
-        chai.request(app)
-            .delete(`/files/${fileId}`)
-            .set('Accept', 'application/json')
-            .end((err, res) => {
-                expect(res.status).to.equal(200);
-                expect(res.body.data).to.include({});
-                done();
-            });
-    });
+        it('Should update file by id', async () => {
+            await chai.request(app).post('/users').set('Accept', 'application/json').send(newUser).then(async () => {
+                await chai.request(app).post('/users/login').set('Accept', 'application/json').send(newUser).then(async (res) => {
+                    token = res.body.data;
+                    await chai.request(app).post('/files').set('Accept', 'application/json').set('x-access-token', token).send(newFile).then(async (response) => {
+                        const fileid = response.body.data.id;
+                        newFile.content = "update test file content";
+                        await chai.request(app).put('/files/' + fileid).set('Accept', 'application/json').set('x-access-token', token).send(newFile).then((response) => {
+                            expect(response.status).to.equal(200);
+                            expect(response.body.message).to.include("File updated");
+                            expect(response.body.data.content).to.include(newFile.content);
+                        });
+                    })
+                })
+            })
+        });
+    })
 });
