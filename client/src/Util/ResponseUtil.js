1ff07ab6d30add86b9b8fb133bb032be1283d5fd
GAnjali <anjali.gaddam98@gmail.com>
2019-12-19 13:27:36 +0530

    Add ResponseUtil for handling the service responses

---
+++ client/src/Util/ResponseUtil.js
@@ -0,0 +1,37 @@
+export default class ResponseUtil {
+
+    constructor() {
+        this.statusCode = null;
+        this.type = null;
+        this.data = null;
+        this.message = null;
+    }
+
+    getResponse(response) {
+        if (response.status === 200 && response.data.status === 'success') {
+            if (response.data.data !== undefined) {
+                this.setSuccess(response.data.message, response.data.data);
+            } else {
+                this.setSuccess(response.data.message, []);
+            }
+        } else {
+            this.setError(response.message);
+        }
+        return {
+            status: this.type,
+            message: this.message,
+            data: this.data,
+        };
+    }
+
+    setSuccess(message, data) {
+        this.message = message;
+        this.data = data;
+        this.type = 'success';
+    }
+
+    setError(message) {
+        this.message = message;
+        this.type = 'error';
+    }
+}
