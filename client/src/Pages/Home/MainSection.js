de9c92132cbd5ca84dec4615029b5194ff4e9c0a
GAnjali <anjali.gaddam98@gmail.com>
2019-12-19 19:20:46 +0530

    Add text message when no contents found

--- client/src/Pages/Home/MainSection.js
+++ client/src/Pages/Home/MainSection.js
@@ -3,12 +3,19 @@ import Folders from "../Folder/Folders";
 import Files from "../File/Files";
 
 const MainSection = (props) => {
+    if (props.folders.length !== 0 || props.files.length != 0) {
+        return (
+            <div className={"main-section"}>
+                {props.folders !== undefined && (props.folders).length > 0 &&
+                <Folders folders={props.folders} handleFolderClick={props.handleFolderClick}/>}
+                {props.files !== undefined && (props.files).length > 0 &&
+                <Files files={props.files} handleFileClick={props.handleFileClick} handleSave={props.handleSave}/>}
+            </div>
+        )
+    }
     return (
         <div className={"main-section"}>
-            {props.folders !== undefined && (props.folders).length > 0 &&
-            <Folders folders={props.folders} handleFolderClick={props.handleFolderClick}/>}
-            {props.files !== undefined && (props.files).length > 0 &&
-            <Files files={props.files} handleFileClick={props.handleFileClick} handleSave={props.handleSave}/>}
+            <span className={"emptysection"}>No contents found</span>
         </div>
     )
 };
