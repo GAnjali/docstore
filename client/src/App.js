7ec75efb78b4125eaa6e255c77f25af4e80f96d4
GAnjali <anjali.gaddam98@gmail.com>
2019-12-19 11:19:47 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./Pages/Home/index";
-import Login from "./Pages/Login/index";
-import Signup from "./Pages/Signup/index";
+import Login from "./Pages/Login";
+import Signup from "./Pages/Signup";
 
 function App() {
   return (
