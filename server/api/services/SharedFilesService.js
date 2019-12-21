b660837a31ce432b08d9b3e991b1ea0a35d9b64e
GAnjali <anjali.gaddam98@gmail.com>
2019-12-21 21:28:43 +0530

    Fix issue of wrong data of shared files from getSharedFiles API service

--- server/api/services/SharedFilesService.js
+++ server/api/services/SharedFilesService.js
@@ -1,43 +1,22 @@
-import database from "../../db/models";
+import ShareService from "./ShareService";
+import FileService from "./FileService";
 
 class SharedFilesService {
-    static async getSharedFiles(userId) {
-        try {
-            const sharedFiles = [];
-            await database.share.findAll({
-                where: {userid: userId},
-                include: database.file
-            })
-                .then((shares) => {
-                    const resObj = shares.map(share => {
-                        return Object.assign(
-                            {},
-                            {
-                                share_id: share.id,
-                                file_id: share.fileid,
-                                share_type: share.sharetype,
-                                user_id: share.userid,
-                                sharedFile: Object.assign(
-                                    {},
-                                    {
-                                        id: share.file.id,
-                                        userid: share.file.userid,
-                                        name: share.file.name,
-                                        content: share.file.content,
-                                        parentfolderid: share.file.parentfolderid
-                                    }
-                                )
-                            })
-                    });
-                    resObj.map(share => {
-                        sharedFiles.push(share.sharedFile)
-                    });
-                })
-            return sharedFiles;
-        } catch (error) {
-            throw error;
-        }
+  static async getSharedFiles(userId) {
+    try {
+      const shares = await ShareService.getAllByUser(userId);
+      Promise.all(
+        shares.map(async share => {
+          const file = await FileService.getOne(share.fileid);
+          return file.dataValues;
+        })
+      ).then(result => {
+        return result;
+      });
+    } catch (error) {
+      throw error;
     }
+  }
 }
 
 export default SharedFilesService;
