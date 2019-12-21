c463ce42192e272af77ba6c269215e6c20467ad9
GAnjali <anjali.gaddam98@gmail.com>
2019-12-21 12:30:11 +0530

    Refactor signup page

---
+++ client/src/AppConstants.js
@@ -0,0 +1,10 @@
+export const HOME_URL = "/";
+export const LOGIN_URL = "/login";
+export const SIGNUP_URL = "/signup";
+
+export const APP_NAME = "Docstore";
+export const USER_ADDED = "User Added!";
+export const TOKEN_GENERATED = "Token generated";
+export const EMAIL_PLACEHOLDER = "Username";
+export const PASSWORD_PLACEHOLDER = "Password";
+export const STATUS_CODE_201 = 201;
