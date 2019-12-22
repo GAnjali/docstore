0fbe2e0ea7a82f344d33a32b6ad85ba0d706c402
GAnjali <anjali.gaddam98@gmail.com>
2019-12-22 11:09:21 +0530

    Fix getFolders API issue with due to equals operator

--- server/api/services/FileService.js
+++ server/api/services/FileService.js
@@ -1,74 +1,85 @@
-import database from "../../db/models/index"
+import database from "../../db/models/index";
 
 class FileService {
-    static async getAll(userId) {
-        try {
-            return await database.file.findAll({
-                where: {userid: userId}
-            });
-        } catch (error) {
-            throw error;
-        }
+  static async getAll(userId) {
+    try {
+      return await database.file.findAll({
+        where: { userid: userId }
+      });
+    } catch (error) {
+      throw error;
     }
+  }
 
-    static async getAllByParent(userId, parentFolderId) {
-        try {
-            return await database.file.findAll({
-                where: {userid: userId, parentfolderid: parentFolderId != 0 ? parentFolderId : null}
-            });
-        } catch (error) {
-            throw error;
+  static async getAllByParent(userId, parentFolderId) {
+    try {
+      return await database.file.findAll({
+        where: {
+          userid: userId,
+          parentfolderid: parseInt(parentFolderId) !== 0 ? parentFolderId : null
         }
+      });
+    } catch (error) {
+      throw error;
     }
+  }
 
-    static async add(file) {
-        try {
-            return await database.file.create(file);
-        } catch (error) {
-            throw error;
-        }
+  static async add(file) {
+    try {
+      return await database.file.create(file);
+    } catch (error) {
+      throw error;
     }
+  }
 
-    static async getOne(id) {
-        try {
-            return await database.file.findOne({
-                where: {id: Number(id)}
-            });
-        } catch (error) {
-            throw error;
-        }
+  static async getOne(id) {
+    try {
+      return await database.file.findOne({
+        where: { id: Number(id) }
+      });
+    } catch (error) {
+      throw error;
     }
+  }
 
-    static async update(id, updateFile) {
-        try {
-            const fileToUpdate = await database.file.findOne({
-                where: {id: Number(id)}
-            });
-            if (!fileToUpdate) return null;
-            if (fileToUpdate.name === updateFile.name)
-                await database.file.update({content: updateFile.content}, {where: {id: Number(id)}});
-            else
-                await database.file.update({
-                    name: updateFile.name,
-                    content: updateFile.content
-                }, {where: {id: Number(id)}});
-            return updateFile;
-        } catch (error) {
-            throw error;
-        }
+  static async update(id, updateFile) {
+    try {
+      const fileToUpdate = await database.file.findOne({
+        where: { id: Number(id) }
+      });
+      if (!fileToUpdate) return null;
+      if (fileToUpdate.name === updateFile.name)
+        await database.file.update(
+          { content: updateFile.content },
+          { where: { id: Number(id) } }
+        );
+      else
+        await database.file.update(
+          {
+            name: updateFile.name,
+            content: updateFile.content
+          },
+          { where: { id: Number(id) } }
+        );
+      return updateFile;
+    } catch (error) {
+      throw error;
     }
+  }
 
-    static async delete(id) {
-        try {
-            const fileToDelete = await database.file.findOne({where: {id: Number(id)}});
-            if (!fileToDelete) return null;
-            return await database.file.destroy({
-                where: {id: Number(id)}
-            });
-        } catch (error) {
-            throw error;
-        }
+  static async delete(id) {
+    try {
+      const fileToDelete = await database.file.findOne({
+        where: { id: Number(id) }
+      });
+      if (!fileToDelete) return null;
+      return await database.file.destroy({
+        where: { id: Number(id) }
+      });
+    } catch (error) {
+      throw error;
     }
+  }
 }
 
 export default FileService;
--- server/api/services/FolderService.js
+++ server/api/services/FolderService.js
@@ -1,55 +1,61 @@
 import database from "../../db/models/index";
 
 class FolderService {
-    static async add(newFolder) {
-        try {
-            return await database.folder.create(newFolder);
-        } catch (error) {
-            throw error;
-        }
+  static async add(newFolder) {
+    try {
+      return await database.folder.create(newFolder);
+    } catch (error) {
+      throw error;
     }
+  }
 
-    static async getByName(folderName) {
-        try {
-            return await database.folder.findOne({
-                where: {name: folderName}
-            });
-        } catch (error) {
-            throw error;
-        }
+  static async getByName(folderName) {
+    try {
+      return await database.folder.findOne({
+        where: { name: folderName }
+      });
+    } catch (error) {
+      throw error;
     }
+  }
 
-    static async getAllByParent(userId, parentFolderId) {
-        try {
-            return await database.folder.findAll({
-                where: {userid: userId, parentfolderid: parentFolderId !== 0 ? parentFolderId : null}
-            });
-        } catch (error) {
-            throw error;
+  static async getAllByParent(userId, parentFolderId) {
+    try {
+      const result = await database.folder.findAll({
+        where: {
+          userid: userId,
+          parentfolderid: parseInt(parentFolderId) !== 0 ? parentFolderId : null
         }
+      });
+      return result;
+    } catch (error) {
+      throw error;
     }
+  }
 
-    static async getAll(userId) {
-        try {
-            return await database.folder.findAll({
-                where: {userid: userId}
-            });
-        } catch (error) {
-            throw error;
-        }
+  static async getAll(userId) {
+    try {
+      return await database.folder.findAll({
+        where: { userid: userId }
+      });
+    } catch (error) {
+      throw error;
     }
+  }
 
-    static async delete(folderId) {
-        try {
-            const fileToDelete = await database.folder.findOne({where: {id: Number(folderId)}});
-            if (!fileToDelete) return null;
-            return await database.folder.destroy({
-                where: {id: Number(folderId)}
-            });
-        } catch (error) {
-            throw error;
-        }
+  static async delete(folderId) {
+    try {
+      const fileToDelete = await database.folder.findOne({
+        where: { id: Number(folderId) }
+      });
+      if (!fileToDelete) return null;
+      return await database.folder.destroy({
+        where: { id: Number(folderId) }
+      });
+    } catch (error) {
+      throw error;
     }
+  }
 }
 
 export default FolderService;
