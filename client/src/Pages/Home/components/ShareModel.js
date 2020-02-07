2014d66824451cd71c02452d6a308519aab58faf
ganjali <anjali.gaddam98@gmail.com>
2020-02-07 16:50:04 +0530

    Modify ShareModel.js into pure Component

--- client/src/Pages/Home/components/ShareModel.js
+++ client/src/Pages/Home/components/ShareModel.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import {
   EDIT,
   EMAIL_PLACEHOLDER,
@@ -9,45 +9,56 @@ import {
   VIEW
 } from "../../../AppConstants";
 
-const ShareModel = props => {
-  const { handleClose, handleInput, handleShareType, handleShare } = props;
-  let style = {};
-  if (props.show === true) {
-    style = { display: "block" };
+class ShareModel extends Component {
+  constructor(props) {
+    super(props);
   }
-  return (
-    <div id="myModal" className="sharingmodal" style={style}>
-      <div className="sharingmodal-content">
-        <span className="close" onClick={handleClose}>
-          &times;
-        </span>
-        <span className={"sharingmodel-heading"}> {SHARE_WITH_OTHERS} </span>
-        <span className={"people-span"}> {PEOPLE} </span>
-        <div className={"sharingmodel-body"}>
-          <textarea
-            className={"sharewith-textarea"}
-            name={"sharingWithUser"}
-            placeholder={EMAIL_PLACEHOLDER}
-            onChange={handleInput}
-          />
-          <div className="sharetype-dropdown">
-            <button className="dropbtn">{SHARE_TYPE}</button>
-            <div className="dropdown-content">
-              <p onClick={handleShareType} id={"View"}>
-                {VIEW}
-              </p>
-              <p onClick={handleShareType} id={"Modify"}>
-                {EDIT}
-              </p>
+
+  render() {
+    const {
+      handleClose,
+      handleInput,
+      handleShareType,
+      handleShare
+    } = this.props;
+    let style = {};
+    if (this.props.show === true) {
+      style = { display: "block" };
+    }
+    return (
+      <div id="myModal" className="sharingmodal" style={style}>
+        <div className="sharingmodal-content">
+          <span className="close" onClick={handleClose}>
+            &times;
+          </span>
+          <span className={"sharingmodel-heading"}> {SHARE_WITH_OTHERS} </span>
+          <span className={"people-span"}> {PEOPLE} </span>
+          <div className={"sharingmodel-body"}>
+            <textarea
+              className={"sharewith-textarea"}
+              name={"sharingWithUser"}
+              placeholder={EMAIL_PLACEHOLDER}
+              onChange={handleInput}
+            />
+            <div className="sharetype-dropdown">
+              <button className="dropbtn">{SHARE_TYPE}</button>
+              <div className="dropdown-content">
+                <p onClick={handleShareType} id={"View"}>
+                  {VIEW}
+                </p>
+                <p onClick={handleShareType} id={"Modify"}>
+                  {EDIT}
+                </p>
+              </div>
             </div>
           </div>
+          <button className={"share-button"} onClick={handleShare}>
+            {SHARE}
+          </button>
         </div>
-        <button className={"share-button"} onClick={handleShare}>
-          {SHARE}
-        </button>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default ShareModel;
